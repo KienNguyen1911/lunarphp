45cfcc09ebc9d2901410e266b8d3874ac9fccf86
Nguyễn Đồng Kiên <94209892+KienNguyen1911@users.noreply.github.com>
2024-12-06 00:01:29 +0700

    update: change rate limit

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -42,10 +42,10 @@ class RouteServiceProvider extends ServiceProvider
     protected function configureRateLimiting(): void
     {
         RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+            return Limit::perMinute(30)->by($request->user()?->id ?: $request->ip());
         });
         RateLimiter::for('web', function (Request $request) {
-            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+            return Limit::perMinute(30)->by($request->user()?->id ?: $request->ip());
         });
     }
 }
